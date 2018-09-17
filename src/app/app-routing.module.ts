41d9338d6ee8985463e838979267090e3e27aa76
liyiping <liyiping@imzhiliao.com>
2018-09-17 10:16:27 +0800

    提交

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
+import { LayoutComponent } from './core/component/layout/layout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/app/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'app',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: './pages/home/home.module#HomeModule',
+      }
+    ]
+  },
+  
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
