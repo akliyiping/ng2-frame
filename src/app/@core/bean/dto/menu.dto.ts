718f13d713b25aefa9c2244e8677dcd0cd098e9b
liyiping <liyiping@imzhiliao.com>
2018-09-21 14:49:11 +0800

    菜单

--- src/app/@core/bean/dto/menu.dto.ts
+++ src/app/@core/bean/dto/menu.dto.ts
@@ -1,20 +1,10 @@
-export interface Children {
-	children?: any;
-	permissions: string;
-	name: string;
-	checked: boolean;
-	id: string;
-	sort: string;
-	parentId: string;
-}
-
 export interface MenuDto {
-	children: Children[];
-	permissions: string;
-	name: string;
-	checked: boolean;
-	id: string;
-	sort: string;
-	parentId: string;
+  children: MenuDto[];
+  permissions: string;
+  name: string;
+  checked: boolean;
+  id: string;
+  sort: string;
+  parentId: string;
 }
 
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { IndexComponent } from './index/index.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 @NgModule({
   imports: [
     CommonModule,
+    NgZorroAntdModule,
     HomeRoutingModule
   ],
   declarations: [IndexComponent]
